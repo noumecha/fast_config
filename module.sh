87e769ddd0af761620caf23bd39428b953d5633e
noumecha spaker <noumechaivan@gmail.com>
2023-04-06 15:08:43 +0100

    upd

---
+++ module.sh
@@ -0,0 +1,3 @@
+mkdir src src/Plugin src/Plugin/Layout src/Plugin/Layout/Sections src/Plugin/Layout/Teasers
+mkdir wbu-atomique-theme wbu-atomique-theme/src wbu-atomique-theme/src/js wbu-atomique-theme/src/js/sections wbu-atomique-theme/src/js/teasers
+mkdir icones icones/sections icones/teasers
